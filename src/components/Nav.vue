eba0939f055294da58661b7d8379c711f25072d3
James Summers <227wLmrlcY0a>
2020-06-15 08:50:10 -0500

    update nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="nav">
     <p>developer // writer \\ musician</p>
+    <ul>
+      <li>home</li>
+      <li>projects</li>
+      <li>resume</li>
+      <li>contact</li>
+    </ul>
   </div>
 </template>
 
@@ -10,9 +16,23 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 p {
-  margin: 0 auto;
+  font-size: 22px;
+  margin: 50px auto 0;
   width: fit-content;
+}
+ul {
+  display: block;
+  margin: 50px auto 0;
+  padding: 0;
+  width: fit-content;
+  li {
+    cursor: pointer;
+    display: inline-block;
+    font-size: 22px;
+    list-style: none;
+    margin: 0 22px;
+  }
 }
 </style>
