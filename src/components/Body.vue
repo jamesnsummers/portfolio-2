537182017511ae950b734c9cb94526bb40b51bea
James Summers <227wLmrlcY0a>
2020-06-15 08:49:52 -0500

    add file

---
+++ src/components/Body.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="body">
+    <a href="https://austinjustice.org/">
+      helloworld0
+    </a>
+    <a href="https://www.aclu.org/">
+      helloworld1
+    </a>
+    <a href="https://www.naacp.org/">
+      helloworld2
+    </a>
+    <a href="https://blacklivesmatter.com/">
+      helloworld3
+    </a>
+    <a href="https://www.youtube.com/watch?v=krfcq5pF8u8">
+      helloworld4
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Body',
+}
+</script>
+
+<style scoped lang="scss">
+.body {
+  font-size: 22px;
+  margin: 50px auto 0;
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+}
+</style>
