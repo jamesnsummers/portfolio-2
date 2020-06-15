d43937a7a1d1d90b228b10561d7480e87d38fba2
James Summers <227wLmrlcY0a>
2020-06-15 08:51:01 -0500

    add Body && update styles

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,37 @@
     <Toggle />
     <img alt="JnS logo" src="./assets/jns-logo-transparent.png">
     <Nav msg="a portfolio"/>
+    <Body />
   </div>
 </template>
 
 <script>
 import Nav from './components/Nav.vue'
 import Toggle from './components/Toggle.vue'
+import Body from './components/Body.vue'
 
 export default {
   name: 'App',
   components: {
     Toggle,
     Nav,
+    Body,
   }
 }
 </script>
 
 <style lang="scss">
 html {
-  background: #FFF;
+  background: #FFFFFF;
   img {
+    height: 150px;
     filter: invert(15%);
+    width: auto;
   }
   &.dm {
-    background: #35373b;
+    background: #35373B;
     #app {
-      color: #FFF;
+      color: #FFFFFF;
     }
     img {
       filter: invert(1);
@@ -36,7 +41,7 @@ html {
   }
 }
 #app {
-  color: #2c3e50;
+  color: #2C3E50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   margin-top: 60px;
   text-align: center;
